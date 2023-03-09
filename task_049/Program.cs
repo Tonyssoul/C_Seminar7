373cf45728d05ab33f6fde29b996e143348b64bc
Anton Kargapolov <igloooooook@gmail.com>
2023-03-09 14:30:48 +0500

    task 049

---
+++ task_049/Program.cs
@@ -0,0 +1,64 @@
+﻿// Задача 49: Задайте двумерный массив. Найдите элементы, у которых оба индекса нечётные, 
+// и замените эти элементы на их квадраты.
+// Например, изначально массив
+//  выглядел вот так:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+
+int ReadInt(string text)
+{
+    System.Console.Write(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int[,] GenerateMatrix(int rows, int cols)
+{
+    Random rand = new Random();
+    int[,] matrix = new int[rows, cols];
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rand.Next(0, 11);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            System.Console.Write(matrix[i, j] + "\t");
+        }
+        System.Console.WriteLine();
+    }
+
+}
+
+void ZamenaNechet(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (i % 2 == 1 && j % 2 == 1)
+            {
+                matrix[i, j] = matrix[i, j] * matrix[i, j];
+            }
+        }
+    }
+}
+
+int rows = ReadInt("Введите колличество строк матрицы ");
+int cols = ReadInt("Введите колличество столбцов матрицы ");
+System.Console.WriteLine();
+var myMatrix = GenerateMatrix(rows, cols);
+PrintMatrix(myMatrix);
+System.Console.WriteLine();
+ZamenaNechet(myMatrix);
+PrintMatrix(myMatrix);
