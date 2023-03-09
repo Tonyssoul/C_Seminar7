9afab4898214d89008bbffecfef0c0ac45d972e1
Anton Kargapolov <igloooooook@gmail.com>
2023-03-09 14:48:28 +0500

    task 051

---
+++ task_051/Program.cs
@@ -0,0 +1,65 @@
+﻿// Задача 51: Задайте двумерный массив. Найдите сумму элементов, находящихся на главной диагонали 
+// (с индексами (0,0); (1;1) и т.д.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Сумма элементов главной диагонали: 1+9+2 = 12
+
+
+int ReadInt(string text)
+{
+    System.Console.Write(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int[,] GenerateMatrix(int rows, int cols)
+{
+    Random rand = new Random();
+    int[,] matrix = new int[rows, cols];
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i,j]= rand.Next(0,11);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            System.Console.Write(matrix[i,j]+"\t");
+        }
+        System.Console.WriteLine();
+    }
+    
+}
+
+int SummElements(int[,] matrix)
+{
+    int Summ = 0;
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if(i==j)
+            {
+                Summ += matrix[i,j];
+            }
+        }
+    }
+    return Summ;
+}
+
+int rows = ReadInt("Введите колличество строк матрицы ");
+int cols = ReadInt("Введите колличество столбцов матрицы ");
+System.Console.WriteLine();
+var myMatrix = GenerateMatrix(rows, cols);
+PrintMatrix(myMatrix);
+System.Console.WriteLine($"Сумма элементов главной диагонали равна {SummElements(myMatrix)}");
